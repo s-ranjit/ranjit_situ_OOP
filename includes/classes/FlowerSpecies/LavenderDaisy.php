9baa3611e37f218eec3dd6704c25468845728d8c
Situ Ranjit <situranjit6@gmail.com>
2026-03-11 18:19:46 -0400

    Add FlowerSpecies subclass and loop through methods
    in this commit, i added FlowerSpecies subclass and used foreach loop to loop through their methods

---
+++ includes/classes/FlowerSpecies/LavenderDaisy.php
@@ -0,0 +1,16 @@
+<?php
+namespace MyProject\FlowerSpecies;
+
+use MyProject\Daisy;
+
+class LavenderDaisy extends Daisy {
+    public function bloom() {
+        echo "<p>It blooms with soft purple petals.</p>";
+    }
+    public function grow() {
+        echo "<p>It grows well in sunny gardens.</p>";
+    }
+    public function fragrance() {
+        echo "<p>It has a light and pleasant fragrance,</p>";
+    }
+}
---
+++ includes/classes/FlowerSpecies/TigerLily.php
@@ -0,0 +1,16 @@
+<?php
+namespace MyProject\FlowerSpecies;
+
+use MyProject\Lily;
+
+class TigerLily extends Lily {
+   public function bloom() {
+        echo "<p>It blooms in the summer with bright orange petals.</p>";
+    }
+    public function grow() {
+        echo "<p>It grows in warm, sunny places.</p>";
+    }
+    public function fragrance() {
+        echo "<p>It has a light and pleasant fragrance.</p>";
+    }
+}
